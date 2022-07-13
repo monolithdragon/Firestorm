5064cc5a736470327bed58a3cb4895f7ca8bdbd9
monolithdragon <joo.lajos76@gmail.com>
2022-07-13 17:58:50 +0200

    Created databease class and interface

---
+++ src/Firestorm/DatabaseConnection/Database.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_type=1);
+
+namespace Firestorm\DatabaseConnection;
+
+use Firestorm\DatabaseConnection\Exception\DatabaseException;
+use PDO;
+use PDOException;
+
+class Database implements DatabaseInterface
+{
+    /**     
+     * @var PDO
+     */
+    protected PDO $pdo;
+
+    /**
+     * @var array
+     */
+    protected array $credentials;
+
+    /**
+     * Constructor class
+     *
+     * @param  array $credentials
+     */
+    public function __construct(array $credentials)
+    {
+        $this->credentials = $credentials;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function open(): PDO
+    {
+        try {
+            $options = [
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_PERSISTENT => true,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ];
+
+            $this->pdo = new PDO(
+                $this->credentials['dsn'],
+                $this->credentials['username'],
+                $this->credentials['password'],
+                $options
+            );
+        } catch(PDOException $ex) {
+            throw new DatabaseException($ex->getMessage(), $ex->getCode());            
+        }
+
+        return $this->pdo;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function close(): void
+    {
+        $this->pdo = null;
+    }
+}
---
+++ src/Firestorm/DatabaseConnection/DatabaseInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_type=1);
+
+namespace Firestorm\DatabaseConnection;
+
+use PDO;
+
+interface DatabaseInterface
+{
+    /**
+     * Create a new database connection
+     *
+     * @return PDO
+     */
+    public function open(): PDO;
+
+    /**
+     * Close database connection
+     *
+     * @return void
+     */
+    public function close(): void;
+}
