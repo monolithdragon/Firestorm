4a73cff42b40d4e8d374fc5a81cb57e16dea637e
monolithdragon <joo.lajos76@gmail.com>
2022-07-14 23:04:04 +0200

    Added core exceptions

---
+++ src/Firestorm/Core/Exception/CoreInvalidArgumentException.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace Firestorm\Core\Exception;
+
+use InvalidArgumentException;
+
+class CoreInvalidArgumentException extends InvalidArgumentException
+{}
---
+++ src/Firestorm/Core/Exception/CoreLogicException.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace Firestorm\Core\Exception;
+
+use LogicException;
+
+class CoreLogicException extends LogicException
+{}
